25d5f017f7340a851e7b9d2ae6be88f3eba41330
natalyMaia <121266917+natalyMaia@users.noreply.github.com>
2023-04-09 14:27:23 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,88 @@
+var balloon,balloonImage1,balloonImage2;
+var database;
+var height;
+
+function preload(){
+  bg =loadImage("Images/cityImage.png");
+  balloonImage1=loadAnimation("Images/HotAirBallon01.png");
+  balloonImage2=loadAnimation("Images/HotAirBallon01.png","Images/HotAirBallon01.png",
+  "Images/HotAirBallon01.png","Images/HotAirBallon02.png","Images/HotAirBallon02.png",
+  "Images/HotAirBallon02.png","Images/HotAirBallon03.png","Images/HotAirBallon03.png","Images/HotAirBallon03.png");
+  }
+
+//Função para definir o ambiente inicial
+function setup() {
+  database=firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,650,250,650);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  var balloonHeight=database.ref('balloon/height');
+  balloonHeight.on("value",readHeight, showError);
+  textSize(20); 
+}
+
+// função para exibir a interface do usuário
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    updateHeight(-10,0);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    updateHeight(10,0);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+  }
+  else if(keyDown(UP_ARROW)){
+    updateHeight(0,-10);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale=balloon.scale -0.005;
+  }
+  else if(keyDown(DOWN_ARROW)){
+    updateHeight(0,+10);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale=balloon.scale+0.005;
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use as setas para mover o balão de ar quente!",40,40);
+
+}
+
+
+function updateHeight(x,y){
+  database.ref('balloon/height').update({
+    'x': height.x + x ,
+    'y': height.y + y
+  })
+}
+
+
+
+
+function readHeight(data){
+  height = data.val();
+  console.log(height.x);
+  balloon.x = height.x;
+  balloon.y = height.y;
+
+  /*const firebaseConfig = {
+    apiKey: "AIzaSyBJHJ_0vh5TeooEB6TTa6XQ7veruDtihaA",
+    authDomain: "aula36-26c31.firebaseapp.com",
+    databaseURL: "https://aula36-26c31-default-rtdb.firebaseio.com",
+    projectId: "aula36-26c31",
+    storageBucket: "aula36-26c31.appspot.com",
+    messagingSenderId: "489881937143",
+    appId: "1:489881937143:web:bb1e8a2fdb73d185f75ff2"
+  };*/
+}
+
+function showError(){
+  console.log("Erro ao escrever no banco de dados");
+}
